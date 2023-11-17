0d11b6f2526576b36795eae8532a27a95d2a9b70
Aleksandr <96977037+Balaberd@users.noreply.github.com>
2023-11-17 12:10:35 +0500

    cors

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-  origin: process.env.CLIENT_URL,
-  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  allowedHeaders: 'X-Requested-With,content-type'
-}));
+// app.use(cors({
+//   credentials: true,
+//   origin: process.env.CLIENT_URL,
+//   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+//   allowedHeaders: 'X-Requested-With,content-type'
+// }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use("/api", router);
 app.use(errorMiddleware);
 
